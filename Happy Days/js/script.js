516c66e8e6f5384959e5f957247002141b34ba07
Bahr23 <35177244+Bahr23@users.noreply.github.com>
2019-01-24 18:31:35 +0300

    Add files via upload

---
+++ Happy Days/js/script.js
@@ -0,0 +1,13 @@
+(function($){
+	$(document).ready(function(){
+		$('.ca_home_main_block').slick({
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 3500,
+			arrows: false,
+			dots: false,
+		});
+	});
+})(jQuery);
